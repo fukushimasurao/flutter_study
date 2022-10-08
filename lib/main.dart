8662c499d47fb68da6d5d28aee177b745c22f3da
HAYATO FUKUSHIMA <46878156+fukushimasurao@users.noreply.github.com>
2022-10-09 02:15:25 +0900

    ざっくりとウジェットの練習

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,74 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text(
+              'こんにちは',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: const [
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+                TweetTile(),
+              ],
+            ),
+          )),
+    );
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://1.bp.blogspot.com/-AXPN-GR4IFc/V8VFDMcn7tI/AAAAAAAA9YQ/q4fSpPcdRmwtk5TKxGgwSLnijDQ-7VZSACLcB/s800/onigiri_nori.png'),
+          ),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: const [
+                  Text('こんぶ @ Flutter大学'),
+                  SizedBox(width: 8),
+                  Text('2022/05/05'),
+                ],
+              ),
+              const SizedBox(height: 4),
+              const Text('最高でした。'),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.favorite_border),
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
